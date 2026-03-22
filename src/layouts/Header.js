8b80bff849d94337826806fe9f63c58d04a7e0ee
Vladislav Nesterenko <vladicknesterenko@gmail.com>
2026-03-22 14:19:02 +0200

    Add header interface Add styling for interface

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -1,4 +1,42 @@
 export const Header = () => {
 
-    return `<p>HEADER</p>`;
+    return `
+    <div class="header-wrapper">
+    
+        <div></div>
+        
+        <div class="header-content">
+        
+            <!--      TOP ROW      -->
+            <div class="header-row-top">
+            
+                <span>Login / Register</span>
+            </div>
+            
+            <!--      MIDDLE ROW      -->
+            <div class="header-row-mid">
+            
+                <span>Left Info</span>
+                <span>Right Info</span>
+            </div>
+            
+            <!--     BOTTOM ROW       -->
+            <div class="header-row-bot">
+            
+                <div class="left-item">Icon</div>
+                
+                    <nav>
+                        <a href="/" data-link>Home</a>
+                        <a href="/intro" data-link>Intro</a>
+                        <a href="/loan-calculator" data-link>Loan Calculator</a>
+                        <a href="/extra" data-link>Extra</a>
+                    </nav>
+                    
+                <div class="right-item">Search</div>
+            </div>
+        </div>
+        
+        <div></div>
+    </div>
+    `;
 };
