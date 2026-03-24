fd508ce3950ce5362cafa5f377d668e542fb8b69
kartjem <artyom.kulikovski@gmail.com>
2026-03-24 23:40:16 +0200

    finished footer

--- src/partials/Footer.js
+++ src/partials/Footer.js
@@ -8,30 +8,43 @@ export const Footer = () => {
             <div class="footer-content-col">
             
                 <h3>Aleksei Kurõljov</h3>
+                <p>Batch 6</p>
+                <p><a href="#">LinkedIn</a></p>
                 <p>Focus: Frontend</p>
+                <p>GAME JAM?</p>
+                <p>EBS Defence AI?</p>
             </div>
         
             <div class="footer-content-col">
             
                 <h3>Vladislav Nesterenko</h3>
+                <p>Batch 6</p>
+                <p><a href="#">LinkedIn</a></p>
                 <p>Focus: Backend</p>
+                <p>CV.PRO</p>
             </div>
         
             <div class="footer-content-col">
             
                 <h3>Artjom Kulikovski</h3>
+                <p>Batch 5</p>
+                <p><a href="#">LinkedIn</a></p>
                 <p>Focus: Frontend</p>
+                <p>OPTIKA WEB</p>
             </div>
             
             <div class="footer-content-col">
             
-                <h3>Team activities</h3>
+                <h3>Team activities & Hackathons:</h3>
+                <p>Eesti Disaini Keskus Accelerator (#1)</p>
                 <p>MVP: TasteTea.eu</p>
+                <p>Tehnopol: Film & Multimedia (#2)</p>
+                <p>Extra</p>
             </div>
         </div>
         
         <div class="footer-legal">
-            Team
+            HELLOMBA LEGAL SMTH AMIGOS
         </div>
     </div>
     `;
